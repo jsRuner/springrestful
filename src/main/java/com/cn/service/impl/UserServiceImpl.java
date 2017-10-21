ce01cb65682aa18d970fcd37926d04045812b912
北岸的云 <hiwower@gmail.com>
2017-10-21 16:47:26 +0800

    项目初始化。

---
+++ src/main/java/com/cn/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.cn.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cn.base.BasicMapper;
+import com.cn.base.BasicServiceImpl;
+import com.cn.dao.UserMapper;
+import com.cn.model.User;
+import com.cn.service.UserService;
+
+@Service("UserService")
+public class UserServiceImpl extends BasicServiceImpl implements UserService {
+	
+	@Autowired
+	UserMapper userMapper;
+
+	@Override
+	public BasicMapper getMapper() {
+		return userMapper;
+	}
+
+	public void saveOrUpdate(User user) {
+		// 将事务放在controller层是不合理的想法，业务逻辑理应放在service层
+		// ，一旦某个环节出错，相关save&update&delete操作应该回滚
+		userMapper.save(user);
+
+		user.setAccount("fzm");
+		userMapper.update(user);
+	}
+	
+}
